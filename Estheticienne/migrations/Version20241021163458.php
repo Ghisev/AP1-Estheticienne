0b1405c90ecf0b3a938f4777686309b0fc2aaacc
QuentinHeyblom <qheyblom@outlook.com>
2024-10-21 20:02:14 +0200

    <Feat>(prestation)
    Intégration de prestation en base de données

--- Estheticienne/migrations/Version20241021163458.php
+++ Estheticienne/migrations/Version20241021163458.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241021161527 extends AbstractMigration
+final class Version20241021163458 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,12 @@ final class Version20241021161527 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE avis DROP categorie');
-        $this->addSql('ALTER TABLE prestation ADD categorie INT NOT NULL');
+        $this->addSql('ALTER TABLE prestation ADD categorie_img INT NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE avis ADD categorie INT NOT NULL');
-        $this->addSql('ALTER TABLE prestation DROP categorie');
+        $this->addSql('ALTER TABLE prestation DROP categorie_img');
     }
 }
