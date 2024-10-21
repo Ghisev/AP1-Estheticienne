dcc35452f24cbc233e26c0f242cf032ab9d26671
QuentinHeyblom <qheyblom@outlook.com>
2024-10-21 17:06:55 +0200

    <Fix>(Avis)
    avis fonctionne enfin !!!

--- Estheticienne/src/Controller/AvisController.php
+++ Estheticienne/src/Controller/AvisController.php
@@ -4,11 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-
-use App\Repository\AvisRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Avis;
 use App\Form\AvisType;
@@ -18,32 +15,37 @@ class AvisController extends AbstractController
     #[Route('/avis', name: 'app_avis')]
     public function index(Request $request, EntityManagerInterface $em): Response
     {
-        $avisRepository=$em->getRepository(Avis::class);
-        $lesAvis=$avisRepository->findAll();
+        $avisRepository = $em->getRepository(Avis::class);
+        $lesAvis = $avisRepository->findAll();
 
         return $this->render('avis/index.html.twig', [
-            'controller_name' => 'AvisController',
             'lesAvis' => $lesAvis,
         ]);
     }
 
     #[Route('/ajoutAvis', name: 'app_ajoutAvis')]
-    public function ajoutPiece(Request $request, EntityManagerInterface $em): Response
+    public function ajoutAvis(Request $request, EntityManagerInterface $em): Response
     {   
+        // Ensure only logged-in users can add reviews
         $this->denyAccessUnlessGranted('ROLE_USER');
+
         $avis = new Avis();
         $formAjoutAvis = $this->createForm(AvisType::class, $avis);
         
         $formAjoutAvis->handleRequest($request);
 
-        if($formAjoutAvis->isSubmitted() && $formAjoutAvis->isvalid()){
-
+        if ($formAjoutAvis->isSubmitted() && $formAjoutAvis->isValid()) {
+            // Get the current logged-in user
             $user = $this->getUser();
-            $avis->setUserId($user);
 
+            // Set the user for the Avis entity
+            $avis->setUser($user);
+
+            // Persist the Avis entity and save to the database
             $em->persist($avis);
             $em->flush();
 
+            // Add a success message and redirect
             $this->addFlash('success', 'Votre avis a été publié');
 
             return $this->redirectToRoute('app_avis');
@@ -54,30 +56,25 @@ class AvisController extends AbstractController
         ]);
     }
 
-    #[Route('/supprimerAvis{id}', name: 'app_supprimerAvis')]
-    public function supprimmerAvis(Request $request, EntityManagerInterface $em, int $id): Response
+    #[Route('/supprimerAvis/{id}', name: 'app_supprimerAvis')]
+    public function supprimerAvis(Request $request, EntityManagerInterface $em, int $id): Response
     {   
+        // Only admin can delete reviews
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
         $avis = $em->getRepository(Avis::class)->find($id);
         
         if (!$avis) {
-            return $this->redirectToRoute('app_avis'); 
+            return $this->redirectToRoute('app_avis');
         }
 
+        // Check the CSRF token before deleting
         if ($this->isCsrfTokenValid('delete' . $avis->getId(), $request->request->get('_token'))) {
             $em->remove($avis);
             $em->flush();
-        } 
-        else {
-
         }
 
-        $avisRepository=$em->getRepository(Avis::class);
-        $lesAvis=$avisRepository->findAll();
-
-        return $this->render('avis/index.html.twig', [
-            'lesAvis' => $lesAvis,
-        ]);
+        // Redirect to the reviews page after deletion
+        return $this->redirectToRoute('app_avis');
     }
 }
--- Estheticienne/src/Entity/Avis.php
+++ Estheticienne/src/Entity/Avis.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\AvisRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Entity\User;
 
 #[ORM\Entity(repositoryClass: AvisRepository::class)]
 class Avis
@@ -14,9 +15,9 @@ class Avis
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\ManyToOne(inversedBy: 'avis')]
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'avis')]
     #[ORM\JoinColumn(nullable: false)]
-    private ?User $user_id = null;
+    private ?User $user = null;
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $texte = null;
@@ -26,14 +27,14 @@ class Avis
         return $this->id;
     }
 
-    public function getUserId(): ?User
+    public function getUser(): ?User
     {
-        return $this->user_id;
+        return $this->user;
     }
 
-    public function setUserId(?User $user_id): static
+    public function setUser(?User $user): static
     {
-        $this->user_id = $user_id;
+        $this->user = $user;
 
         return $this;
     }
